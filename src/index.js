33fc08c698c88e04c29b895a05173d9bf23f6f0c
Omar-Gebal <2001omaremad@gmail.com>
2022-02-14 02:28:02 +0200

    Bug tracker redux

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("Hello World!");
+import store from "./store";
+import {bugAdded, bugResolved} from "./actions.js";
+
+
+store.subscribe(() =>{
+    console.log(store.getState());
+}) ;
+store.dispatch(bugAdded("Bug 1"));
+
+store.dispatch(bugAdded("Bug 2"))
+
+store.dispatch(bugResolved(1));
